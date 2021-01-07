2a34e54c9c0c55789c013442b91a35d43b6b2bf4
ArtemGits <gitsartem@gmail.com>
2021-01-07 18:56:54 +0300

    wip

--- lib/algorithms/un_weighted_mcmc.ex
+++ lib/algorithms/un_weighted_mcmc.ex
@@ -7,9 +7,9 @@ defmodule DagBlockchain.Algorithms do
   Launch consensus algorithm. Start from genesis. Return two tips for consensus
   """
   #FIXME move to consensus module
-  def un_weighted_mcmc(nodes, links) do
+  def unweighted_mcmc(nodes, links) do
     #TODO elixir way to check if nodes lenght is 0, return then []
-    start = nodes[0]
+    [start | _tail_nodes] = nodes 
     [
       random_walk(links, start),
       random_walk(links, start)
@@ -17,14 +17,26 @@ defmodule DagBlockchain.Algorithms do
   end
 
   def random_walk(links, start) do
-    
+    particle = start
+    walk(links, particle, is_tip?(links, particle))
+  end
+
+  #FIXME more functional way
+  defp walk(links, particle, true) do
+    [head_link, tail_links] =  links
+    particle = head_link.target
+    walk(tail_links, particle, is_tip?(tail_links, head_link.target))
+  end
+
+  defp walk(links, particle, false) do
+    particle
   end
 
   @doc """
   Function to check if node of DAG is a tip. Return boolean.
   """
-  def is_tip?(links, particle) do
-    #check if some of the 
+  def is_tip?(links, node) do
+    Enum.any?(links, fn link -> node == link.target end)
   end
 
   def get_approvers(links, node) do
--- test/tip_selection_test.exs
+++ test/tip_selection_test.exs
@@ -4,6 +4,7 @@ defmodule DagBlockchain.TipsSelectionTest do
   #TODO refactor this
   alias DagBlockchain.Node, as: Node
   alias DagBlockchain.Link, as: Link
+  alias DagBlockchain.Algorithms, as: Algo
   #alias DagBlockchain.Tip, as: Tip
 
  # test "unweightedMCMC algorithm" do
@@ -17,15 +18,27 @@ defmodule DagBlockchain.TipsSelectionTest do
 
   #TODO create test suit!
   test "Dag's transaction generate" do
-    nodes = create_list_of_nodes([], 10)
-    assert length(nodes) == 10
+    nodes = create_list_of_nodes([], 3)
+    #assert length(nodes) == 3
+    IO.inspect(nodes)
 
-    #TODO tips selection algorithm
-    tips = [%Node{name: 11}, %Node{name: 12}]
-    #TODO function to iterate over nodes for
-    create_list_of_links(links, tips, node)
-
+    candidate_links = []
+    #candidate_nodes = Enum.chunk(nodes, 3, 3, [])
+    #IO.inspect(candidate_nodes)
+    candidate_nodes = nodes
+    links = Enum.reduce(candidate_nodes, [], &simulation(&1, &2, candidate_links, candidate_nodes))
+    IO.inspect(links)
   end
+
+  defp simulation(node, result, candidate_links, candidate_nodes) do
+    #TODO tips selection algorithm
+    candidate_links = result
+    IO.inspect(candidate_links)
+    tips = Algo.unweighted_mcmc(candidate_nodes, candidate_links)
+    #TODO function to iterate over nodes for
+    candidate_links ++ create_list_of_links(candidate_links, tips, node)
+  end
+
 
   defp create_list_of_nodes(list,0) do
     list
@@ -37,8 +50,8 @@ defmodule DagBlockchain.TipsSelectionTest do
   end
 
   defp create_list_of_links(list, tips, node) do
-    {first_tip, second_tip} = tips
-    case {lenght(tips) > 0, length(tips) > 1 and first_tip.name != second_tip.name} do
+    [first_tip, second_tip] = tips
+    case {length(tips) > 0, length(tips) > 1 and first_tip.name != second_tip.name} do
       {true, true} ->
         list
         |> push_link(node, first_tip)
